c9777938001ff082c23b17aa0f08ba6b383a23c3
БульбочкаВ <disablemon@icloud.com>
2026-02-24 15:44:18 +0300

    df

---
+++ settings.gradle.kts
@@ -0,0 +1,5 @@
+rootProject.name = "salary-app-diploma"
+
+include("user-service")
+include("api-gateway")
+include("service-registry")
---
+++ user-service/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    `java`
+    id("org.springframework.boot") version "4.0.2" apply false
+    id("io.spring.dependency-management") version "1.1.7" apply false
+}
+
+//subprojects {
+//    apply(plugin = "java")
+//    apply(plugin = "org.springframework.boot")
+//    apply(plugin = "io.spring.dependency-management")
+//
+//    group = "com.salary-app"
+//    version = "1.0.0"
+//
+//    repositories {
+//        mavenCentral()
+//    }
+//
+//    java {
+//        toolchain {
+//            languageVersion.set(JavaLanguageVersion.of(21))
+//        }
+//    }
+//
+//    dependencies {
+//        testImplementation(platform("org.junit:junit-bom:5.10.0"))
+//        testImplementation("org.junit.jupiter:junit-jupiter")
+//    }
+//
+//    tasks.test {
+//        useJUnitPlatform()
+//    }
+//}
